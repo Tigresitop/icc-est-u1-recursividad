d4467803cc1f68413bca52e25f799da2881b316a
John Tigre <cuji309@gmail.com>
2024-10-28 12:53:55 -0500

    Avance

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+            Recursividad rec = new Recursividad();
+            int resultado = rec.factorial(5);
+            System.out.println("El resultado es: "+resultado);
+
+            int  resultado2 = rec.sumaConsecutivos(5);
+            System.out.println("El resultado es: "+resultado2);
+
+            int resultado3 = rec.potencia(5, 3);
+            System.out.println("Elresultado de la potencia es: " +resultado3);
+
+            int  resultado4 = rec.sumaDigitos(456);
+            System.out.println("El resultado de la suma de los digitos es: "+resultado4);
+
+
     }
 }
